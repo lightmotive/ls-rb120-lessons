e7fb5d67a9bb0e286f2bddc26ea07988ad268069
Andre Light <andrelight.al@gmail.com>
2022-04-04 21:53:02 +0000

    Complete 02.2

--- lesson02/02_lecture_classes_and_objects.rb
+++ lesson02/02_lecture_classes_and_objects.rb
@@ -1,18 +1,25 @@
 # frozen_string_literal: true
 
 class Person
-  attr_accessor :name
+  attr_accessor :first_name, :last_name
 
-  def initialize(name)
-    self.name = name
+  def initialize(first_name)
+    self.first_name = first_name
+    self.last_name = ''
+  end
+
+  def name
+    return first_name if last_name.empty?
+
+    "#{first_name} #{last_name}"
   end
 end
 
 # Modify the class definition from above to facilitate the following methods.
 
 bob = Person.new('Robert')
-bob.name                  # => 'Robert'
-bob.first_name            # => 'Robert'
-bob.last_name             # => ''
+p bob.name == 'Robert'                  # => 'Robert'
+p bob.first_name == 'Robert'            # => 'Robert'
+p bob.last_name == '' # => ''
 bob.last_name = 'Smith'
-bob.name                  # => 'Robert Smith'
+p bob.name == 'Robert Smith'                  # => 'Robert Smith'
