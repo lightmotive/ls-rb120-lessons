3ebd41b514599252150e6401973ac1a66d767329
Andre Light <andrelight.al@gmail.com>
2022-04-04 21:45:05 +0000

    Initialize 02

---
+++ lesson02/02_lecture_classes_and_objects.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Given the below usage of the Person class, code the class definition.
+
+bob = Person.new('bob')
+bob.name                  # => 'bob'
+bob.name = 'Robert'
+bob.name                  # => 'Robert'
