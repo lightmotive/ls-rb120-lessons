df2f645d83ebff2e0d0aff1ed626a8408f249371
Andre Light <andrelight.al@gmail.com>
2022-04-04 22:19:29 +0000

    Complete 02.3

--- lesson02/02_lecture_classes_and_objects.rb
+++ lesson02/02_lecture_classes_and_objects.rb
@@ -4,21 +4,33 @@ class Person
   attr_accessor :first_name, :last_name
 
   def initialize(name)
-    name_parts = name.split
-    self.first_name = name_parts.first
-    self.last_name = name_parts.size > 1 ? name_parts[1..-1] : ''
+    self.name = name
   end
 
   def name
     "#{first_name} #{last_name}".strip
   end
-end
 
-# Modify the class definition from above to facilitate the following methods.
+  def name=(name)
+    name_parts = name.split
+    self.first_name = name_parts.empty? ? '' : name_parts.first
+    self.last_name = name_parts.size > 1 ? name_parts[1..-1] : ''
+  end
+end
 
 bob = Person.new('Robert')
 p bob.name == 'Robert'                  # => 'Robert'
 p bob.first_name == 'Robert'            # => 'Robert'
 p bob.last_name == '' # => ''
 bob.last_name = 'Smith'
-p bob.name == 'Robert Smith'                  # => 'Robert Smith'
+p bob.name == 'Robert Smith' # => 'Robert Smith'
+
+# Now create a smart name= method that can take just a first name or a full
+# name, and knows how to set the first_name and last_name appropriately.
+bob.name = 'John Adams'
+bob.first_name            # => 'John'
+bob.last_name             # => 'Adams'
+
+bob.name = ''
+p bob.first_name == ''
+p bob.last_name == ''
