650341b430571db8f645cc6ef1b033e65bfd3759
Andre Light <andrelight.al@gmail.com>
2022-04-04 21:47:34 +0000

    Complete 02

--- lesson02/02_lecture_classes_and_objects.rb
+++ lesson02/02_lecture_classes_and_objects.rb
@@ -2,7 +2,15 @@
 
 # Given the below usage of the Person class, code the class definition.
 
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name
+  end
+end
+
 bob = Person.new('bob')
-bob.name                  # => 'bob'
+p bob.name == 'bob' # => 'bob'
 bob.name = 'Robert'
-bob.name                  # => 'Robert'
+p bob.name == 'Robert'                  # => 'Robert'
