a19544cb1f8f95184e2cdbcae3810e0925a0d157
Andre Light <andrelight.al@gmail.com>
2022-04-23 18:58:13 +0000

    Implement `display_winner` method (basic)
    - Messy implementation to follow walkthrough for now.

--- lesson02/14_rps_walkthrough/rps/game.rb
+++ lesson02/14_rps_walkthrough/rps/game.rb
@@ -17,7 +17,40 @@ module RPS
       puts 'Thanks for playing Rock, Paper, Scissors! Bye.'
     end
 
-    def display_winner; end
+    def 
+
+    def display_winner
+      puts "You chose #{human.move}."
+      puts "The computer chose #{computer.move}."
+
+      # There's a much better way to implement this, but I'll implement it
+      # after the walkthrough:
+
+      human_won = -> { puts 'You won!' }
+      human_lost = -> { puts 'You lost.' }
+      game_tied = -> { puts 'Tie!' }
+
+      case human.move
+      when 'rock'
+        if computer.move == 'scissors'
+          human_won.call
+        else
+          (computer.move == 'rock' ? game_tied.call : human_lost.call)
+        end
+      when 'paper'
+        if computer.move == 'rock'
+          human_won.call
+        else
+          (computer.move == 'paper' ? game_tied.call : human_lost.call)
+        end
+      when 'scissors'
+        if computer.move == 'paper'
+          human_won.call
+        else
+          (computer.move == 'scissors' ? game_tied.call : human_lost.call)
+        end
+      end
+    end
 
     def play
       display_welcome_message
