71509e2d9fe6d1e49fed987a49b2769a99b8a138
Andre Light <andrelight.al@gmail.com>
2022-05-01 21:40:29 +0000

    Add class extension note

--- lesson02/14_rps_walkthrough/rps/moves/move.rb
+++ lesson02/14_rps_walkthrough/rps/moves/move.rb
@@ -3,6 +3,7 @@ module RPS
     require_relative 'move_base'
 
     # Move for gameplay.
+    # Extend or inherit this class for stateful/complex moves.
     class Move < MoveBase
       def initialize(move)
         super(move.key, move.data)
