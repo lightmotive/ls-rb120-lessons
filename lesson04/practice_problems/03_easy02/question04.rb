3501426523243d754c106b95a7bfd3a5ab872f01
Andre Light <andrelight.al@gmail.com>
2022-05-31 21:37:27 +0000

    Lesson 4, Easy 02, Question 4
    - Implement simplification.

--- lesson04/practice_problems/03_easy02/question04.rb
+++ lesson04/practice_problems/03_easy02/question04.rb
@@ -1,18 +1,9 @@
 class BeesWax
+  attr_accessor :type
+
   def initialize(type)
     @type = type
   end
-
-  def type
-    @type
-  end
-
-  def type=(t)
-    @type = t
-  end
-  
-  # The methods defined on lines 6 and 10 can be simplified to 1 line:
-  # `attr_accessor :type`
 
   def describe_type
     puts "I am a #{@type} of Bees Wax"
