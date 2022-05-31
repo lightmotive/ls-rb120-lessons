2f66f1083b1cd40808f27084dd68b8b9ba451b13
Andre Light <andrelight.al@gmail.com>
2022-05-31 21:40:01 +0000

    Lesson 4, Easy 02, Question 4
    - Use "getter" method instead of direct ivar ref (best practice).

--- lesson04/practice_problems/03_easy02/question04.rb
+++ lesson04/practice_problems/03_easy02/question04.rb
@@ -6,6 +6,9 @@ class BeesWax
   end
 
   def describe_type
-    puts "I am a #{@type} of Bees Wax"
+    puts "I am a #{type} of Bees Wax"
+    # Here, we also want to use the `type` "getter" method instead of
+    # referencing the instance variable directly, which yields more
+    # maintainable code.
   end
 end
