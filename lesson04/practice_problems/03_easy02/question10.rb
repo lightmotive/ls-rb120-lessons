dee660d69d346aa5d6268f5527acb2861c2a6a1f
Andre Light <andrelight.al@gmail.com>
2022-05-31 22:01:23 +0000

    Lesson 4, Easy 02, Question 10

---
+++ lesson04/practice_problems/03_easy02/question10.rb
@@ -0,0 +1,8 @@
+# Some benefits of OOP in Ruby:
+# - Organize code into logical groups with domain-specific names, which
+#   simplifies program organization while improving flow and conceptualization.
+#   - That enables encapsulation, which has these benefits:
+#     - Data protection that supports sepration of concerns.
+#     - Abstraction that supports comprehension and code reusability.
+# - Reuse code through inheritance (classes, Is-a/hierarchical relationship)
+#   or mixins (modules, Has-a/non-hierarchical relationship).
