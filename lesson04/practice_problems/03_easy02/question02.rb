7b0c4ff345483c3bae557166e3df9bb7e132a03d
Andre Light <andrelight.al@gmail.com>
2022-05-31 21:32:51 +0000

    Lesson 4, Easy 02, Question 2
    - Expand answer.

--- lesson04/practice_problems/03_easy02/question02.rb
+++ lesson04/practice_problems/03_easy02/question02.rb
@@ -19,3 +19,8 @@ puts trip.predict_the_future
 # Line 18 invokes the `predict_the_future` method inherited from `Oracle`.
 # That method invokes the `choices` method in `RoadTrip`, which overrides
 # `choices` in `Oracle`.
+# Ruby looks for methods in the order defined in Class#ancestors. In the
+# example above, the lookup path is `RoadTrip.ancestors`.
+p RoadTrip.ancestors
+# Notice that RoadTrip is first; Ruby executes the first `choices` method
+# found in a Class or Module in that lookup path.
