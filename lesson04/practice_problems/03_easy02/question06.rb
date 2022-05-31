cc6777e7d9ab2cf102baa0788f1462b096a3638c
Andre Light <andrelight.al@gmail.com>
2022-05-31 21:42:23 +0000

    Lesson 4, Easy 02, Question 6

---
+++ lesson04/practice_problems/03_easy02/question06.rb
@@ -0,0 +1,12 @@
+class Television
+  def self.manufacturer
+    # method logic
+  end
+
+  def model
+    # method logic
+  end
+end
+
+# Television::manufacturer is a class method and is invoked like this:
+Television.manufacturer
