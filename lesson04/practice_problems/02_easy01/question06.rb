cfadd118089acaf19603c3289a3dc2fec8278b25
Andre Light <andrelight.al@gmail.com>
2022-05-31 21:13:52 +0000

    Lesson 4, Easy 01, Question 6
    - Explore ancestors and method owners.

--- lesson04/practice_problems/02_easy01/question06.rb
+++ lesson04/practice_problems/02_easy01/question06.rb
@@ -4,4 +4,22 @@ class Cube
   def initialize(volume)
     @volume = volume
   end
+
+  def to_s
+    "#{self.class} with volume of #{volume}"
+  end
 end
+
+cube = Cube.new(9)
+
+puts cube.to_s
+puts cube.class.to_s
+puts cube.class.name
+puts cube.class.class
+p cube.class.class.ancestors
+p cube.methods.sort
+p cube.method(:volume).owner
+p cube.method(:to_s).owner # => `Kernel` when object doesn't implement `to_s`.
+# Why isn't `to_s`'s owner `Object` when not implemented?
+p cube.to_s
+p cube.class.ancestors
