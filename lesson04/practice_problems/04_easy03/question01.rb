0ab5dbf4948a61b9ff40a176f81ef65b86dd1be0
Andre Light <andrelight.al@gmail.com>
2022-05-31 22:55:25 +0000

    Lesson 4, Easy 03, Question 1

---
+++ lesson04/practice_problems/04_easy03/question01.rb
@@ -0,0 +1,36 @@
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet('Hello')
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet('Goodbye')
+  end
+end
+
+# case 1:
+hello = Hello.new # new instance of `Hello`
+hello.hi # => Hello\n
+
+# case 2:
+hello = Hello.new
+hello.bye # NoMethodError
+
+# case 3:
+hello = Hello.new
+hello.greet # ArgumentError
+
+# case 4:
+hello = Hello.new
+hello.greet('Goodbye') # => Goodbye\n
+
+# case 5:
+Hello.hi # NoMethodError
