e723f6f6ed96506a3c871013964d00ece5c28387
Andre Light <andrelight.al@gmail.com>
2022-06-01 16:59:34 +0000

    Lesson 4, Medium 1, Question 5
    - Implement class changes to yield expected output.

--- lesson04/practice_problems/05_medium01/question05.rb
+++ lesson04/practice_problems/05_medium01/question05.rb
@@ -1,7 +1,25 @@
 class KrispyKreme
+  attr_reader :filling_type, :glazing
+
   def initialize(filling_type, glazing)
     @filling_type = filling_type
     @glazing = glazing
+  end
+
+  def filling_type_display
+    return 'Plain' if filling_type.nil?
+
+    filling_type
+  end
+
+  def glazing_display
+    return '' if glazing.nil?
+
+    " with #{glazing}"
+  end
+
+  def to_s
+    filling_type_display + glazing_display
   end
 end
 
@@ -11,17 +29,8 @@ donut3 = KrispyKreme.new(nil, 'sugar')
 donut4 = KrispyKreme.new(nil, 'chocolate sprinkles')
 donut5 = KrispyKreme.new('Custard', 'icing')
 
-puts donut1
-# => "Plain"
-
-puts donut2
-# => "Vanilla"
-
-puts donut3
-# => "Plain with sugar"
-
-puts donut4
-# => "Plain with chocolate sprinkles"
-
-puts donut5
-# => "Custard with icing"
+p donut1.to_s == 'Plain'
+p donut2.to_s == 'Vanilla'
+p donut3.to_s == 'Plain with sugar'
+p donut4.to_s == 'Plain with chocolate sprinkles'
+p donut5.to_s == 'Custard with icing'
