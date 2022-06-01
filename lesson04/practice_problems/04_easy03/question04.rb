94bcf13a154c27c70dca03717750f931f13f78f7
Andre Light <andrelight.al@gmail.com>
2022-06-01 15:29:27 +0000

    Lesson 4, Easy 03, Question 4
    - Be more explicit with method names.

--- lesson04/practice_problems/04_easy03/question04.rb
+++ lesson04/practice_problems/04_easy03/question04.rb
@@ -5,10 +5,10 @@ class Cat
     @type = type
   end
 
-  def to_s
+  def type_display # It's better to be more explicit with methods.
     "I am a #{type} cat."
   end
 end
 
 tabby = Cat.new('tabby')
-puts tabby
+puts tabby.type_display
