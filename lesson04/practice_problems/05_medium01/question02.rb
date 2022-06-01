0d44f4f46c7f1be96e135180f7330cb7fa9270f3
Andre Light <andrelight.al@gmail.com>
2022-06-01 16:46:36 +0000

    Lesson 4, Medium 1, Question 3
    - Implement review suggestions.

--- lesson04/practice_problems/05_medium01/question02.rb
+++ lesson04/practice_problems/05_medium01/question02.rb
@@ -2,16 +2,12 @@ class InvoiceEntry
   attr_reader :quantity, :product_name
 
   def initialize(product_name, number_purchased)
-    @quantity = number_purchased
     @product_name = product_name
+    update_quantity(number_purchased)
   end
 
   def update_quantity(updated_count)
     # prevent negative quantities from being set
-    quantity = updated_count if updated_count >= 0
-    # Code review: `quantity` needs an `@` prefix; otherwise, `quantity`
-    # will be initialized as a local variable.
-    # Note: one should limit instance variable changes to one method for better
-    # code maintainability.
+    @quantity = updated_count if updated_count >= 0
   end
 end
