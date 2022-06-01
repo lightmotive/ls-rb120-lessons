63009e74fc818601e5db127032c6a9a4f14913c3
Andre Light <andrelight.al@gmail.com>
2022-06-01 19:41:52 +0000

    Lesson 4, Hard 1, Question 1
    - Refactor `speed` and `heading` attributes to `Moveable` module.
    - Refactor fuel-related attributes and behaviors to `Fuel` module.

--- lesson04/practice_problems/06_hard01/question01.rb
+++ lesson04/practice_problems/06_hard01/question01.rb
@@ -1,10 +1,26 @@
-class WheeledVehicle
+module Moveable
   attr_accessor :speed, :heading
+end
+
+module Fuel
+  def initialize_fuel(km_traveled_per_liter, liters_of_fuel_capacity)
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Fuel
+  include Moveable
 
   def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
     @tires = tire_array
-    @fuel_efficiency = km_traveled_per_liter
-    @fuel_capacity = liters_of_fuel_capacity
+
+    initialize_fuel(km_traveled_per_liter, liters_of_fuel_capacity)
   end
 
   def tire_pressure(tire_index)
@@ -13,10 +29,6 @@ class WheeledVehicle
 
   def inflate_tire(tire_index, pressure)
     @tires[tire_index] = pressure
-  end
-
-  def range
-    @fuel_capacity * @fuel_efficiency
   end
 end
 
@@ -33,3 +45,26 @@ class Motorcycle < WheeledVehicle
     super([20, 20], 80, 8.0)
   end
 end
+
+class Catamaran
+  include Moveable
+  include Fuel
+
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+
+    initialize_fuel(km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+auto = Auto.new
+puts auto.range
+
+motorcycle = Motorcycle.new
+puts motorcycle.range
+
+catamaran = Catamaran.new(2, 1, 40, 21.0)
+puts catamaran.range
