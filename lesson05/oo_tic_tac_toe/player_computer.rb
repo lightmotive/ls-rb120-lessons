81c7923ab39b86469377fda48f184744803cb4ae
Andre Light <andrelight.al@gmail.com>
2022-06-03 19:13:05 +0000

    Implement `Player` subclasses

---
+++ lesson05/oo_tic_tac_toe/player_computer.rb
@@ -0,0 +1,7 @@
+require_relative 'player'
+
+class PlayerComputer < Player
+  def mark_board(board)
+    # Apply computer logic to mark board and apply mark
+  end
+end
---
+++ lesson05/oo_tic_tac_toe/player_human.rb
@@ -0,0 +1,7 @@
+require_relative 'player'
+
+class PlayerHuman < Player
+  def mark_board(board)
+    # Request input to mark board, validate, and apply mark
+  end
+end
