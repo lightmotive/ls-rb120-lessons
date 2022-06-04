beb0d6ff8ec32ac95b7f985b48e632ec16d2ce07
Andre Light <andrelight.al@gmail.com>
2022-06-04 20:03:29 +0000

    Implement `Board` class with `Grid` module
    - Draw board, mark square, and winning line logic to be added.

--- lesson05/oo_tic_tac_toe/board.rb
+++ lesson05/oo_tic_tac_toe/board.rb
@@ -1,10 +1,32 @@
+require_relative 'grid'
+require_relative 'square'
+
 class Board
+  include Grid
+
+  attr_reader :size
+
   def initialize(size)
     @size = size
+    create_grid(Square)
   end
 
   def draw
     # Output square grid of @size to console...
+  end
+
+  def available_squares
+    grid.each_with_object([]) do |row, available|
+      available.push(*row.reject(&:marked?))
+    end
+  end
+
+  def available_selectors
+    available_squares.map(&:selector)
+  end
+
+  def move_valid?(input)
+    available_selectors.include?(input)
   end
 
   def mark(player, selector)
@@ -13,15 +35,22 @@ class Board
     square.mark(player)
   end
 
-  def winner
-    # Return a winner; nil if no winner
+  def winner?
+    winning_line = self.winning_line()
+    return false if winning_line.empty?
+
+    true
   end
 
   private
 
-  attr_reader :size, :grid
-
   def select_square(selector)
     # Find square in grid using selector
   end
+
+  def winning_line
+    # ...select winning line
+  end
 end
+
+p Board.new(3).available_selectors
