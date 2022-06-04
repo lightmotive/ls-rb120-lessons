61591bff6424c8e8eec6f9d76c2b7682e461b06a
Andre Light <andrelight.al@gmail.com>
2022-06-04 01:45:23 +0000

    Finish `Player` class

--- lesson05/oo_tic_tac_toe/player.rb
+++ lesson05/oo_tic_tac_toe/player.rb
@@ -3,12 +3,15 @@ require_relative 'abstract_not_implemented_error'
 class Player
   attr_reader :name, :mark
 
-  def initialize(name, mark)
+  def initialize(name)
     @name = name
+  end
+
+  def initialize_mark(mark)
     @mark = mark
   end
 
-  # Abstract
+  # Abstract: concrete should accept `board` instance and call `board.mark(...)`.
   def mark_board(_board)
     raise AbstractNotImplementedError
   end
