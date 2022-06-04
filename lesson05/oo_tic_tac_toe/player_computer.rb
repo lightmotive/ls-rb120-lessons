59820f422e2988c59d62421ce37c62ef4457ae14
Andre Light <andrelight.al@gmail.com>
2022-06-04 01:46:32 +0000

    Draft concrete `Player` classes
    - `PlayerHuman` and `PlayerComputer`.

--- lesson05/oo_tic_tac_toe/player_computer.rb
+++ lesson05/oo_tic_tac_toe/player_computer.rb
@@ -1,6 +1,10 @@
 require_relative 'player'
 
 class PlayerComputer < Player
+  def initialize
+    super('Computer')
+  end
+
   def mark_board(board)
     # Apply computer logic to mark board and apply mark
   end
--- lesson05/oo_tic_tac_toe/player_human.rb
+++ lesson05/oo_tic_tac_toe/player_human.rb
@@ -3,5 +3,30 @@ require_relative 'player'
 class PlayerHuman < Player
   def mark_board(board)
     # Request input to mark board, validate, and apply mark
+    print "What's your move? (enter square number) "
+    square_number =
+      loop do
+        input = gets.strip
+        break input.to_i if move_input_valid?(board, input)
+
+        print 'Please enter a valid square number: '
+      end
+
+    board.mark(self, square_number)
+  end
+
+  def self.request_name
+    print "What's your name? "
+
+    loop do
+      name = gets.strip
+      break name unless name.nil? || name.empty?
+    end
+  end
+
+  private
+
+  def move_input_valid?(board, input)
+    board.available_square_selectors.include?(input)
   end
 end
