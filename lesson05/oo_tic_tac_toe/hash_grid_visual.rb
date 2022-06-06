8592ae186ea4406fdc0198f7b3138fe5c38089f3
Andre Light <andrelight.al@gmail.com>
2022-06-06 19:35:19 +0000

    Simplify `HashGridVisual`
    - Don't pass `hash_grid` attributes internally.

--- lesson05/oo_tic_tac_toe/hash_grid_visual.rb
+++ lesson05/oo_tic_tac_toe/hash_grid_visual.rb
@@ -1,6 +1,10 @@
 require_relative 'stdout_format'
 
 class HashGridVisual
+  CELL_WIDTH_PAD = 1
+  CELL_VERTICAL_PAD = 0
+  CELL_WIDTH = (CELL_WIDTH_PAD * 2) + 1
+
   def initialize(hash_grid)
     @hash_grid = hash_grid
   end
@@ -13,27 +17,23 @@ class HashGridVisual
       )}\n"
     end
 
-    puts row_strings.join("#{row_divider(hash_grid.size)}\n").to_s
+    puts row_strings.join("#{row_divider}\n").to_s
   end
 
   private
 
-  SPACE_WIDTH_PADDING = 1
-  SPACE_VERTICAL_PADDING = 0
-  SPACE_WIDTH = (SPACE_WIDTH_PADDING * 2) + 1
-
   attr_reader :hash_grid
 
-  def row_padding(column_count)
-    Array.new(column_count, ' ' * SPACE_WIDTH).join('|')
+  def row_padding
+    Array.new(hash_grid.size, ' ' * CELL_WIDTH).join('|')
   end
 
   def cell_key_display(cell)
-    cell.key.to_s.rjust(SPACE_WIDTH, ' ').italic.gray
+    cell.key.to_s.rjust(CELL_WIDTH, ' ').italic.gray
   end
 
   def cell_display_with_padding(cell, include_cell_numbers)
-    padding = ' ' * SPACE_WIDTH_PADDING
+    padding = ' ' * CELL_WIDTH_PAD
 
     display_string = cell.display
     display_string = display_string.bold unless cell.empty?
@@ -54,22 +54,20 @@ class HashGridVisual
     end.join('|')
   end
 
-  def row_string(cells, include_cell_numbers: false)
-    result = row_marks(
-      cells, include_cell_numbers: include_cell_numbers
-    )
-
-    return result unless SPACE_VERTICAL_PADDING.positive?
-
-    vertical_padding = Array.new(
-      SPACE_VERTICAL_PADDING,
-      row_padding(cells.size)
-    ).join("\n")
-
-    "#{vertical_padding}\n#{result}\n#{vertical_padding}"
+  def vertical_padding
+    Array.new(CELL_VERTICAL_PAD, row_padding).join("\n")
   end
 
-  def row_divider(column_count)
-    Array.new(column_count, '-' * SPACE_WIDTH).join('+')
+  def row_string(cells, include_cell_numbers: false)
+    result = row_marks(cells, include_cell_numbers: include_cell_numbers)
+    return result unless CELL_VERTICAL_PAD.positive?
+
+    vpad = vertical_padding
+
+    "#{vpad}\n#{result}\n#{vpad}"
+  end
+
+  def row_divider
+    Array.new(hash_grid.size, '-' * CELL_WIDTH).join('+')
   end
 end
