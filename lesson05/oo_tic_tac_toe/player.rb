40c752a04f516a06c40f8c30163a0380ac00f513
Andre Light <andrelight.al@gmail.com>
2022-06-11 22:45:26 +0000

    Add discussion re:  `Player`'s `Board` dependency

--- lesson05/oo_tic_tac_toe/player.rb
+++ lesson05/oo_tic_tac_toe/player.rb
@@ -1,5 +1,32 @@
 require_relative 'abstract_not_implemented_error'
 
+# Tradeoff discussion regarding loosely coupling `Board` and `Player` classes:
+# Ideally, we want to minimize coupling between classes. `Player` needs to be
+# aware of a specific `Board` instance, while `Board` doesn't need to be aware
+# of a specific `Player` instance. Therefore, there is a loose coupling between
+# the two classes, but it's an acceptable tradeoff in this program.
+#
+# Pros:
+# - Player move strategy is encapsulated in one place, minimizing complexity.
+# - No circular dependencies between `Board` and `Player`.
+#
+# Cons:
+# - `Player` needs access to a specific `Board` instance that `Game` cannot
+#   replace, thus requiring slightly more complicated Game + players
+#   instantiation.
+#
+# The `Player` subclass must be aware of the `Game`'s `Board` instance to
+# execute a specific strategy. Significant move strategy logic is currently
+# implemented in the `PlayerComputer` class only, but similar logic could apply
+# to a future "Suggestions" feature that gives the user hints about how to move.
+#
+# Therefore, it makes sense to put player strategy logic directly in `Player`
+# subclasses in order to avoid an overly complex design (in this case)
+# that would require several additional classes, e.g.:
+# - `PlayerStrategy` superclass and player-specific subclasses.
+#   - That class would still need to be aware of `Board`.
+# - `Player` returns `PlayerStrategy` subclass type or instance.
+# - `MoveOrchestrator` class that centralizes board awareness and marking.
 class Player
   attr_reader :name, :board, :mark
 
