1b37fada268738023b245eb5de5063717989129a
Andre Light <andrelight.al@gmail.com>
2022-06-15 22:58:59 +0000

    Implement `StandardDeckCard` skeleton

--- lesson05/oo_twenty_one/standard_deck_card.rb
+++ lesson05/oo_twenty_one/standard_deck_card.rb
@@ -1,2 +1,15 @@
 class StandardDeckCard
+  attr_accessor :is_face_up
+  attr_reader :suit, :rank
+
+  alias face_up? is_face_up
+
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+  end
+
+  def to_s
+    # different display depending on is_face_up value...
+  end
 end
