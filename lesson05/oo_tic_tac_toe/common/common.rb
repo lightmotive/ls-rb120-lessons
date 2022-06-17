3472e50509e32b3481408a988517c4403c391f64
Andre Light <andrelight.al@gmail.com>
2022-06-17 23:30:59 +0000

    Add note regarding future private Gem migration

--- lesson05/oo_tic_tac_toe/common/common.rb
+++ lesson05/oo_tic_tac_toe/common/common.rb
@@ -1,3 +1,5 @@
+# NOTE: Other programs in this lesson reference this file. To be migrated to a private gem.
+# [Private Gems starting point](https://medium.com/@bernardomg/build-a-private-ruby-gem-b5f53dc2e911).
 require_relative 'validation_error'
 require_relative 'prompt/prompt'
 require_relative 'messages/messages'
